d5fc5c22ae25a7899ec8bb29df81fa8aedccc386
Manny <egalli64@gmail.com>
2020-06-03 08:11:48 +0200

    package name fix + minor cleanup

--- src/main/java/francesca/S04.java
+++ src/main/java/francesca/S04.java
@@ -1,67 +1,65 @@
-package m2ex;
+package francesca;
 
 public class S04 {
-	/**
-	 * Add up all the numbers in the passed closed interval
-	 * 
-	 * @param first the left limit
-	 * @param last  the right limit
-	 * @return the sum of all the numbers, or zero
-	 */
-	public static long sum(int first, int last) {
-		long res = 0;
-		
-		if (last < first)
-			return res;
-		
-		for (int i = first; i <= last; i++) {
-			res += i;
-		}
-		
-		return Math.abs(res);
-	}
+    /**
+     * Add up all the numbers in the passed closed interval
+     * 
+     * @param first the left limit
+     * @param last  the right limit
+     * @return the sum of all the numbers, or zero
+     */
+    public static long sum(int first, int last) {
+        long res = 0;
 
-	/**
-	 * Add up only the even numbers in the passed closed interval
-	 * 
-	 * @param first the left limit
-	 * @param last  the right limit
-	 * @return the sum of all the even numbers, or zero
-	 */
-	public static long evenSum(int first, int last) {
-		long res = 0;
-		
-		if (last < first)
-			return res;
-		
-		for(int i = first; i <= last; i++) {
-			if(i % 2 == 0)
-				res += i;
-		}
-		return res;
-	}
+        if (last < first)
+            return res;
 
-	/**
-	 * Square root using Newton method
-	 * Epsilon should be set to 0.001
-	 *  
-	 * @param value
-	 * @return calculated square root
-	 */
-	public static double sqrt(double value) {
-		if(value == 0)
-			return 0;
-		
-		double x = value / 2;
-		double res = 1/2 * (x + value/x);
-		
-		while(Math.abs(res - x) < 0.001) {
-			x = res;
-			res = 1/2 * (x + value / x);
-		}
-		
-		return res;
-		
+        for (int i = first; i <= last; i++) {
+            res += i;
+        }
+
+        return Math.abs(res);
+    }
+
+    /**
+     * Add up only the even numbers in the passed closed interval
+     * 
+     * @param first the left limit
+     * @param last  the right limit
+     * @return the sum of all the even numbers, or zero
+     */
+    public static long evenSum(int first, int last) {
+        long res = 0;
+
+        if (last < first)
+            return res;
+
+        for (int i = first; i <= last; i++) {
+            if (i % 2 == 0)
+                res += i;
+        }
+        return res;
+    }
+
+    /**
+     * Square root using Newton method Epsilon should be set to 0.001
+     * 
+     * @param value
+     * @return calculated square root
+     */
+    public static double sqrt(double value) {
+        if (value == 0)
+            return 0;
+
+        double x = value / 2;
+        double res = 1 / 2 * (x + value / x);
+
+        while (Math.abs(res - x) < 0.001) {
+            x = res;
+            res = 1 / 2 * (x + value / x);
+        }
+
+        return res;
 
 //	    value = Math.abs(value);
 //		if (value == 0)
@@ -77,29 +75,29 @@ public class S04 {
 //		}
 //
 //		return newroot;
-	}
+    }
 
-	/**
+    /**
      * Square root using Newton method
      * 
-	 * @param value
-	 * @param epsilon stop iterating when gain is less than this value
-	 * @return calculated square root
-	 */
+     * @param value
+     * @param epsilon stop iterating when gain is less than this value
+     * @return calculated square root
+     */
     public static double sqrt(double value, double epsilon) {
-    	if(value == 0)
-			return 0;
-		
-		double x = value / 2;
-		double res = 1/2 * (x + value/x);
-		
-		while(Math.abs(res - x) < epsilon) {
-			x = res;
-			res = 1/2 * (x + value / x);
-		}
-		
-		return res;
-		
+        if (value == 0)
+            return 0;
+
+        double x = value / 2;
+        double res = 1 / 2 * (x + value / x);
+
+        while (Math.abs(res - x) < epsilon) {
+            x = res;
+            res = 1 / 2 * (x + value / x);
+        }
+
+        return res;
+
 //    	value = Math.abs(value);
 //		if (value == 0)
 //			return 0;
@@ -114,70 +112,70 @@ public class S04 {
 //		}
 //
 //		return newroot;
-    	
+
     }
-	
-	/**
-	 * Factorial
-	 * 
-	 * @param value
-	 * @return factorial of input value, or zero
-	 */
-	public static long factorial(int value) {
-		long res = 1;
-		if(value == 0) //0! = 1
-			return res;
-		else if(value < 0) //-n! = 0
-			res = 0;
-		
-		for(int i = value; i > 0; i--) { //n(n-1)(n-2)...1
-			res *= i;
-		}
-		return res;
-	}
 
-	/**
-	 * Fibonacci number (0, 1, 1, 2, 3, 5, 8, �)
-	 * 
-	 * @param value
-	 * @return the Fibonacci number of value, or zero
-	 */
-	public static long fibonacci(int value) { //0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233,377
-		long res = 0;
-		
-		if(value <= 0) {
-			res = 0;
-		}else if(value == 1 || value == 2) {
-			res = 1;
-		}else {
-			long f0 = 0;
-			long f1 = 1;
-			
-			for(int i = 2; i <= value; i++) {
-				res = f0 + f1; 
-				f0 = f1; 
-				f1 = res; 
-			}
-		}
+    /**
+     * Factorial
+     * 
+     * @param value
+     * @return factorial of input value, or zero
+     */
+    public static long factorial(int value) {
+        long res = 1;
+        if (value == 0) // 0! = 1
+            return res;
+        else if (value < 0) // -n! = 0
+            res = 0;
 
-		return res;
-	}
+        for (int i = value; i > 0; i--) { // n(n-1)(n-2)...1
+            res *= i;
+        }
+        return res;
+    }
 
-	/**
-	 * Multiplication table
-	 * 
-	 * @param value
-	 * @return The multiplication table for value, when possible
-	 */
-	public static int[][] multiplicationTable(int value) {
-		int[][] result = new int[value][value];
-		
-		for(int i = 0; i < value; i++) {
-			for(int j = 0; j < value; j++) {
-				result[i][j] = (i+1) * (j+1);
-			}
-		}
-		
-		return result;
-	}
+    /**
+     * Fibonacci number (0, 1, 1, 2, 3, 5, 8, �)
+     * 
+     * @param value
+     * @return the Fibonacci number of value, or zero
+     */
+    public static long fibonacci(int value) { // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233,377
+        long res = 0;
+
+        if (value <= 0) {
+            res = 0;
+        } else if (value == 1 || value == 2) {
+            res = 1;
+        } else {
+            long f0 = 0;
+            long f1 = 1;
+
+            for (int i = 2; i <= value; i++) {
+                res = f0 + f1;
+                f0 = f1;
+                f1 = res;
+            }
+        }
+
+        return res;
+    }
+
+    /**
+     * Multiplication table
+     * 
+     * @param value
+     * @return The multiplication table for value, when possible
+     */
+    public static int[][] multiplicationTable(int value) {
+        int[][] result = new int[value][value];
+
+        for (int i = 0; i < value; i++) {
+            for (int j = 0; j < value; j++) {
+                result[i][j] = (i + 1) * (j + 1);
+            }
+        }
+
+        return result;
+    }
 }
--- src/main/java/francesca/S05.java
+++ src/main/java/francesca/S05.java
@@ -1,38 +1,38 @@
-package m2ex;
+package francesca;
 
 public class S05 {
-	/**
-	 * Reverse a string
-	 * 
-	 * @param s
-	 * @return the input reversed
-	 */
-	public static String reverse(String s) {
+    /**
+     * Reverse a string
+     * 
+     * @param s
+     * @return the input reversed
+     */
+    public static String reverse(String s) {
 
-		StringBuilder res = new StringBuilder(s.length());
-		
-		for(int i = s.length() - 1; i >= 0; i--) {
-			res.append(s.charAt(i));
-		}
-		return res.toString();
-	}
+        StringBuilder res = new StringBuilder(s.length());
 
-	/**
-	 * Check if the parameter is a palindrome
-	 * 
-	 * @param s
-	 * @return true if the parameter is a palindrome
-	 */
-	public static boolean isPalindrome(String s) {
-		String contrario = reverse(s);
-		boolean pal = true;
-		
-		for(int i = 0; i < s.length(); i++) {
-			if(!s.equals(contrario))
-				pal = false;
-		}
-		return pal;
-		
+        for (int i = s.length() - 1; i >= 0; i--) {
+            res.append(s.charAt(i));
+        }
+        return res.toString();
+    }
+
+    /**
+     * Check if the parameter is a palindrome
+     * 
+     * @param s
+     * @return true if the parameter is a palindrome
+     */
+    public static boolean isPalindrome(String s) {
+        String contrario = reverse(s);
+        boolean pal = true;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!s.equals(contrario))
+                pal = false;
+        }
+        return pal;
+
 //		StringBuilder res = new StringBuilder("");
 //		
 //		for (int i = s.length() - 1; i >= 0; i--) {
@@ -40,96 +40,97 @@ public class S05 {
 //		}
 //
 //		return res.toString();
-	}
+    }
 
-	/**
-	 * Remove vowels from the parameter
-	 * 
-	 * @param s
-	 * @return a string, same of input but without vowels
-	 */
-	public static String removeVowels(String s) {
-		StringBuilder res = new StringBuilder();
-		
-		for(int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
-					||  c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
-			else {
-				res.append(c);
-			}
-		}
+    /**
+     * Remove vowels from the parameter
+     * 
+     * @param s
+     * @return a string, same of input but without vowels
+     */
+    public static String removeVowels(String s) {
+        StringBuilder res = new StringBuilder();
 
-		return res.toString();
-	}
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O'
+                    || c == 'U')
+                ;
+            else {
+                res.append(c);
+            }
+        }
 
-	/**
-	 * Convert from binary to decimal
-	 * 
-	 * @param s a string that contains the binary representation of an integer
-	 * @return the converted integer
-	 */
-	public static int bin2dec(String s) {
-		// [1][0][0][0][1]
-	    // 43_210
-	    // 2
-		int res = 0;
-		
-		for(int i = 0; i < s.length(); i++) {
-			if(s.charAt(i) == '1') {
-				res += Math.pow(2, i);
-			}
-		}
-		
-		return res;
-	}
+        return res.toString();
+    }
 
-	/**
-	 * Reverse an array of integers
-	 * 
-	 * @param data
-	 * @return a new array holding the same elements of input, in reversed order
-	 */
-	public static int[] reverse(int[] data) {
-		int[] result = new int[data.length];
+    /**
+     * Convert from binary to decimal
+     * 
+     * @param s a string that contains the binary representation of an integer
+     * @return the converted integer
+     */
+    public static int bin2dec(String s) {
+        // [1][0][0][0][1]
+        // 43_210
+        // 2
+        int res = 0;
 
-		for(int i = 0; i < data.length; i++) {
-			result[i] = data[data.length -i-1];
-		}
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                res += Math.pow(2, i);
+            }
+        }
 
-		return result;
-	}
+        return res;
+    }
 
-	/**
-	 * Calculate the average
-	 * 
-	 * @param data
-	 * @return the average
-	 */
-	public static double average(int[] data) {
-		double res = 0.0;
-		int n = data.length;
-		
-		for(int i = 0; i < n; i++) {
-			res += data[i];
-		}
-		
-		return res/n;
-	}
+    /**
+     * Reverse an array of integers
+     * 
+     * @param data
+     * @return a new array holding the same elements of input, in reversed order
+     */
+    public static int[] reverse(int[] data) {
+        int[] result = new int[data.length];
 
-	/**
-	 * Find the largest value
-	 * 
-	 * @param data
-	 * @return the largest value
-	 */
-	public static int max(int[] data) {
-		int max = data[0];
-		
-		for(int i = 0; i < data.length; i++) {
-			if(data[i] > max)
-				max = data[i];
-		}
-		return max;
-	}
+        for (int i = 0; i < data.length; i++) {
+            result[i] = data[data.length - i - 1];
+        }
+
+        return result;
+    }
+
+    /**
+     * Calculate the average
+     * 
+     * @param data
+     * @return the average
+     */
+    public static double average(int[] data) {
+        double res = 0.0;
+        int n = data.length;
+
+        for (int i = 0; i < n; i++) {
+            res += data[i];
+        }
+
+        return res / n;
+    }
+
+    /**
+     * Find the largest value
+     * 
+     * @param data
+     * @return the largest value
+     */
+    public static int max(int[] data) {
+        int max = data[0];
+
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] > max)
+                max = data[i];
+        }
+        return max;
+    }
 }
