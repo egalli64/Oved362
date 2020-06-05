28692fb9b767d35f66d38c2b6c26be3f4185ff4c
Davide <d.depasquale94@gmail.com>
2020-06-06 00:17:07 +0200

    Resolved some exercises + relative tests

--- src/main/java/davide/S06.java
+++ src/main/java/davide/S06.java
@@ -17,7 +17,22 @@ public class S06 {
 	 * @return true if value is prime number
 	 */
 	public static boolean isPrime(int value) {
-		return false;
+		value = Math.abs(value);
+
+		if (value < 2) {
+			return false;
+		} else if (value <= 3) {
+			return true;
+		} else if (value % 2 == 0) {
+			return false;
+		} else {
+			for (int i = 3; i < value; i += 2) {
+				if (value % i == 0) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 
 	/**
@@ -26,33 +41,80 @@ public class S06 {
 	 * @return return true if value is Armstrong number
 	 */
 	public static boolean isArmstrong(int value) {
-		return false;
+		value = Math.abs(value);
+		int len = String.valueOf(value).length();
+
+		if (value < 10) {
+			return true;
+		} else {
+			int sum = 0;
+
+			while (value > 0) {
+				sum += Math.pow(value % 10, len);
+				value /= 10;
+			}
+
+			if (value == sum) {
+				return true;
+			} else {
+				return false;
+			}
+		}
 	}
 
 	/**
 	 * 
 	 * @param s (a short sentence)
-	 * @return true if input string is using every letter of a given alphabet at
+	 * @return true if input string is using every letter of a given alphabet (italian) at
 	 *         least once
 	 * 
 	 *         Input -> "Che tempi brevi zio, quando solfeggi" Output -> true
 	 */
 	public static boolean isPangram(String s) {
-		return false;
+		int[] lettersIndex = new int[21];
+		char[] input = s.toCharArray();
+
+		for (int i = 0; i < input.length; i++) {
+			int digit = Character.getNumericValue(input[i]);
+			if (digit >= 10 && digit <= 18) {
+				lettersIndex[digit - 10]++;
+			} else if (digit >= 21 && digit <= 31) {
+				lettersIndex[digit - 12]++;
+			} else if (digit == 35) {
+				lettersIndex[20]++;
+			}
+		}
+		
+		for (int i : lettersIndex) {
+			System.out.println(i);
+		}
+		
+		for (int i = 0; i < lettersIndex.length; i++) {
+			if (lettersIndex[i] == 0) {
+				return false;
+			}
+		}
+		
+		return true;
 	}
 
 	/**
 	 * 
 	 * @param s1
-	 * @param s2 s1 and s2 must have same lenght
+	 * @param s2 s1 and s2 must have same length
 	 * @return the number of positions at which the corresponding symbols are
 	 *         different. In other words, it measures the minimum number of
 	 *         substitutions required to change one string into the other
 	 * 
-	 *         Input: s1 -> 100, s2 -> 001 Output -> 2
+	 *         Input: s1 -> 100, s2 -> 001 Output: 2
 	 */
 	public static int hammingDistance(String s1, String s2) {
-		return 0;
+		if (s1.equals(s2)) {
+			return 0;
+		} else {
+			//TODO
+			return 1;
+		}
 	}
 
 	/**
--- src/test/java/davide/S06Test.java
+++ src/test/java/davide/S06Test.java
@@ -1,38 +1,109 @@
 package davide;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
 class S06Test {
 
 	@Test
+	void testIsPrimeZero() {
+		boolean actual = S06.isPrime(0);
+		
+		assertThat(actual, is(false));
+	}
+	
+	@Test
+	void testIsPrimeOne() {
+		boolean actual = S06.isPrime(1);
+		
+		assertThat(actual, is(false));
+	}
+	
+	@Test
+	void testIsPrimeTwo() {
+		boolean actual = S06.isPrime(2);
+		
+		assertThat(actual, is(true));
+	}
+	
+	@Test
+	void testIsPrimeThree() {
+		boolean actual = S06.isPrime(3);
+		
+		assertThat(actual, is(true));
+	}
+	
+	@Test
 	void testIsPrime() {
-		fail("Not yet implemented");
+		boolean actual = S06.isPrime(89);
+		
+		assertThat(actual, is(true));
+	}
+	
+	@Test
+	void testIsPrimeNegative() {
+		boolean actual = S06.isPrime(-89);
+		
+		assertThat(actual, is(true));
 	}
 
 	@Test
+	void testIsArmstrongSimple() {
+		boolean actual = S06.isArmstrong(5);
+		
+		assertThat(actual, is(true));
+	}
+	
+	@Test
+	void testIsArmstrongNegative() {
+		boolean actual = S06.isArmstrong(-7);
+		
+		assertThat(actual, is(true));
+	}
+	
+	@Test
 	void testIsArmstrong() {
-		fail("Not yet implemented");
+		boolean actual = S06.isArmstrong(153);
+		
+		assertThat(actual, is(true));
+	}
+	
+	@Test
+	void testIsArmstrongFalse() {
+		boolean actual = S06.isArmstrong(65);
+		
+		assertThat(actual, is(false));
 	}
 
 	@Test
 	void testIsPangram() {
-		fail("Not yet implemented");
+		boolean actual = S06.isPangram("Che tempi brevi zio, quando solfeggi");
+		
+		assertThat(actual, is(true));
+	}
+	
+	@Test
+	void testIsPangram2() {
+		boolean actual = S06.isPangram("Ciao mi chiamo Davide.");
+		
+		assertThat(actual, is(false));
 	}
 
 	@Test
 	void testHammingDistance() {
-		fail("Not yet implemented");
+		fail("Not implemented yet");
 	}
 
 	@Test
 	void testAcronym() {
-		fail("Not yet implemented");
+		fail("Not implemented yet");
 	}
 
 	@Test
 	void testYahtzee() {
-		fail("Not yet implemented");
+		fail("Not implemented yet");
 	}
 }
